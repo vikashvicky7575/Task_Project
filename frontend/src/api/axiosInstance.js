dac1aedd43fed30445a8b9693c0e2ff19038ae17
vikashvicky7575 <mvikash7575@gmail.com>
2026-03-04 11:16:27 +0530

    Updated api url

--- frontend/src/api/axiosInstance.js
+++ frontend/src/api/axiosInstance.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 //Api URL Centralization
 const API = axios.create({
-  baseURL: import.meta.env.API_URL || "http://localhost:5000/api"
+  baseURL: process.env.REACT_APP_API_URL || "http://localhost:5000/api"
 });
 
 //JWT Token stored in localStorage
